aab8f44ce77657575d62771cd2f1df6c12380a5f
lesley <lesleybanadzem2017@gmail.com>
2020-06-18 18:10:46 -0700

    Completed Queue DS using a list as a storage

--- queue/queue.py
+++ queue/queue.py
@@ -1,28 +1,35 @@
 """
 A queue is a data structure whose primary purpose is to store and
-return elements in First In First Out order. 
+return elements in First In First Out order.
 
 1. Implement the Queue class using an array as the underlying storage structure.
    Make sure the Queue tests pass.
 2. Re-implement the Queue class, this time using the linked list implementation
    as the underlying storage structure.
    Make sure the Queue tests pass.
-3. What is the difference between using an array vs. a linked list when 
+3. What is the difference between using an array vs. a linked list when
    implementing a Queue?
-   
+
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-    
+        self.storage = []
+
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.size += 1
+        self.storage.insert(0, value)
 
     def dequeue(self):
-        pass
+        dequeue_item = None
+        if self.size >= 1:
+            dequeue_item = self.storage.pop(self.size - 1)
+            self.size -= 1
+        return dequeue_item
